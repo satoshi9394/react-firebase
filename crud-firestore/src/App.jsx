590406399cbccdf13e78ef29ab732ed11bf627cf
satoshi <angelsb94@gmail.com>
2020-06-23 18:41:57 -0500

    agregando tareas y boorando

--- crud-firestore/src/App.jsx
+++ crud-firestore/src/App.jsx
@@ -3,6 +3,7 @@ import { firebase } from './firebase'
 
 function App() {
   const [ tareas, setTareas ] = useState([])
+  const [ tarea, setTarea ] = useState('') 
 
   React.useEffect( () => {
     const obtenerDatos = async () => {
@@ -20,6 +21,42 @@ function App() {
     obtenerDatos()
   }, [])
 
+  const agregar =  async (e) => {
+    e.preventDefault()
+    if(!tarea.trim()){
+      console.log('esta vacio')
+      return
+    }
+    try {
+      const db = firebase.firestore()
+      const nuevaTarea = { 
+        name: tarea,
+        fecha: Date.now()
+      }
+      const data = await db.collection('tareas').add(nuevaTarea)
+      setTareas([
+        ...tareas,
+        {...nuevaTarea, id: data.id}
+      ])
+      setTarea('')
+    } catch (error) {
+      console.log(error)
+    }
+    console.log(tarea)
+  }
+  const eliminar = async (id) => {
+    try {
+      
+      const db = firebase.firestore()
+      await db.collection('tareas').doc(id).delete()
+      const arrayFiltrado = tareas.filter( item => item.id !== id)
+      setTareas(arrayFiltrado)
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <div className="container mt-3">
       <div className="row">
@@ -29,13 +66,39 @@ function App() {
               tareas.map(item => (
                 <li className="list-group-item" key={item.id}>
                   {item.name}
+                  <button 
+                    className="btn btn-danger btn-sm float-right"
+                    onClick={ () => eliminar(item.id)}
+                  >
+                    Eliminar
+                  </button>
+                  <button 
+                    className="btn btn-warning btn-sm float-right mr-2"
+                  >
+                    Editar
+                  </button>
                 </li>
               ))
             }
           </ul>
         </div>
         <div className="col-md-6">
-          formulario
+          <h3>Formulario</h3>
+          <form onSubmit={agregar}>
+            <input
+              type="text"
+              placeholder="Ingrese tarea"
+              className="form-control mb-2"
+              onChange={e => setTarea(e.target.value)}
+              value={tarea}
+            />
+            <button 
+            className="btn btn-dark btn-block"
+            type="submit"
+            >
+              Agregar
+            </button>
+          </form>
         </div>
       </div>
     </div>
