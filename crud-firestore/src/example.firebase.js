26e67b996cfb665f54735106698aa94f15439fca
satoshi <angelsb94@gmail.com>
2020-06-23 19:27:20 -0500

    agregando ejemplos de auth de firebase

---
+++ crud-firestore/src/example.firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export {firebase}
