4a5c815660e08bc381211cacb6a60e1300a878c2
satoshi <angelsb94@gmail.com>
2020-06-23 20:01:03 -0500

    creando navbar

--- login-udemy-1/src/App.jsx
+++ login-udemy-1/src/App.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
+//components
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola mundo </h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <Switch>
+          <Route path="/" exact>
+            home...
+          </Route>
+          <Route path="/login">
+            login...
+          </Route>
+          <Route path="/admin">
+            admin...
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
