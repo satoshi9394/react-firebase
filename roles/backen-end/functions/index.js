6bed9d6045a3612022917e422f9db6171b66c70c
satoshi <angelsb94@gmail.com>
2020-08-10 18:13:33 -0500

    creando mi primer function

---
+++ roles/backen-end/functions/index.js
@@ -0,0 +1,30 @@
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+
+admin.initializeApp();
+const auth = admin.auth();
+
+exports.agregarAdministrador =  functions.https.onCall(( data, context) => {
+
+  if(context.auth.token.admin !== true) {
+    return { error: 'no tienes los permisos' }
+  }
+  return auth.getUserByEmail(data.email)
+    .then(user => {
+      return auth.setCustomUserClaims(user.uid, { admin: true })
+    })
+    .then(() => {
+      return { message: 'Se creo el administrador'}
+    })
+    .catch( error => {
+      return { error: error}
+    })
+})
